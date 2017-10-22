4c0745541c3cdd8faeba4a507312b596c81f69dd
jpquade <jpquade@gmail.com>
2017-10-22 11:47:06 -0700

    Frequency/Amplitude update
    Added random Frequency/Amplitude generator than can include multiple frequencies and amplitudes into a generated sine wave

--- waveFFT.cpp
+++ waveFFT.cpp
@@ -5,16 +5,19 @@
 #include <cmath>
 #include <iterator>
 #include <string>
+#include <time.h>
 #include "fft/FFTRealFixLen.h"
 
 // function prototypes
 double* sinWaveGenerator(int euclid, double sampleRate);			// sin wave generator
+double* randSinWaveGenerator(int euclid, double sampleRate, int frequencyAdded);	// multiple sin wave generator
 void fileCreation(double value[], int dataAmount, std::string fileName);	// outputs to a file
 void fftConversion();								// converts waveform into fft
 
 int main()
 {
-	fftConversion();
+	srand (time(NULL));	// random seed
+	fftConversion();	// start program
 
 	return 0;
 }
@@ -33,6 +36,33 @@ double* sinWaveGenerator(int euclid, double sampleRate)
 	{		
 		value[i] = amplitude * sin(angularFrequency * sampleFrequency + phase);	// number of cycles per number of sample
 		sampleFrequency += (1 / sampleRate);					// increments the sample frequency
+	}
+
+	return value;
+}
+
+double* randSinWaveGenerator(int euclid, double sampleRate, int frequencyAdded)
+{		
+				
+	double amplitude;				// peak deviation of the function from zero
+	double ordinaryFrequency;			// number of oscillations (cycles) each second
+	double phase = 0;				// phase shift
+	double *value = new double[euclid];		// array of sin wave values
+	double sampleFrequency = 1 / sampleRate;	// sampling frequency rate	
+
+	for(int j = 0; j < frequencyAdded; j++)
+	{
+		ordinaryFrequency = (double)(rand() % 100 + 1);					// random frequency between 1-100
+		std::cout << "Frequency" << j << " " << ordinaryFrequency;		// prints out the current frequency
+		amplitude = (double)(rand() % 35 + 10);					// random frequency between 10-35
+		std::cout << " Amplitude" << j << " " << amplitude << std::endl;			// prints out the current amplitude
+
+		for(int i = 0; i < euclid; i++)
+		{				
+			value[i] += amplitude * sin(2 * M_PI * ordinaryFrequency * sampleFrequency + phase);	// number of cycles per number of sample
+			sampleFrequency += (1 / sampleRate);						// increments the sample frequency			
+		}
+		sampleFrequency = 1 / sampleRate;
 	}
 
 	return value;
@@ -75,25 +105,26 @@ void fftConversion()
 	
 	ffft::FFTRealFixLen <exponentOfTwo> fft;
 	
-	preFFT = sinWaveGenerator(euclid, sampleRate);	// generates test sin wave
-	fileCreation(preFFT, euclid, file1);
+	preFFT = randSinWaveGenerator(euclid, sampleRate, 5);	// generates multiple random sin waves
+
+	fileCreation(preFFT, euclid, file1);			// file for sin waves
 
 	fft.do_fft(postFFT, preFFT);			// performs fft on array
 
 	
-	fileCreation(postFFT, euclid, file2);		// generates initial fft data
+	fileCreation(postFFT, euclid, file2);		// file for initial fft data
 
 	for(int i = 0; i < euclidMod; i++)
 	{
 		bins[i] = (double)i * binFrequency;
 	}
 
-	fileCreation(bins, euclid, file3);		// generates bin size data
+	fileCreation(bins, euclid, file3);		// file for frequency bins
 	
 	for(int i = 1; i < euclidMod; i++)
 	{
-		conversionFFT[i] = sqrt(pow(postFFT[i], 2) + pow((postFFT[(euclid / 2) + i]), 2)) / euclidMod;	// FFT a + bi removes the first and middle value
+		conversionFFT[i] = sqrt(pow(postFFT[i], 2) + pow((postFFT[(euclid / 2) + i]), 2)) / euclidMod;	// FFT (a + bi)  magnitude / numberofbins
 	}	
 	
-	fileCreation(conversionFFT, euclidMod, file4);	// formats and reconfigures fft for analysis	
+	fileCreation(conversionFFT, euclidMod, file4);	// file for aplitudes
 }
